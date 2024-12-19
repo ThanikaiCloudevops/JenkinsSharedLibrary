e6e0c7e2d105ddc877bd7464e6fff674b9406e06
Thanikai <128052851+ThanikaiCloudevops@users.noreply.github.com>
2024-12-19 15:03:10 +0530

    Jenkins Shared Library Concept Working

---
+++ JenkinsFolder/Jenkins_basefile/BaseJenkinsfile.groovy
@@ -0,0 +1,14 @@
+pipelineJob("Base Pipeline env structure") {
+    description("This Job runs the base pipeline structure from a Git repo")
+
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/thanikai-devops/Devops_ansible.git'){
+                    branch ('dev-jenkins')
+                }
+            }
+            scriptPath('jenkinsfile/jenkinsfile')  // Make sure this path is correct
+        }
+    }
+}
